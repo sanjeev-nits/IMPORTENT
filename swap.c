db9f34cb935352c434c2472846f3edb27d326a8d
sanjeev-nits <sanjeevkumar814155@gmail.com>
2024-12-06 08:11:45 +0530

    Add files via upload

--- swap.c
+++ swap.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+int swap(int *,int *);
 
+void main()
+{
+int a=4,b=5;
+
+printf("before swap a=%d and b=%d",a,b);
+swap(&a,&b);
+
+printf("\nafter swap a=%d and b=%d",a,b);
+
+
+}
+
+int swap(int *a,int *b){
+    int temp;
+
+    temp=*a;
+    *a=*b;
+    *b=temp;
+
+}
